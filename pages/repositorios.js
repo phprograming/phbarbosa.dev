e68d7a957e14eb877b0dcbe05532f2f3977ba81e
phprograming <paulo_hbs17@hotmail.com>
2020-09-21 23:59:03 -0300

    Three commit

--- pages/repositorios.js
+++ pages/repositorios.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Index = ({ repos, user }) => {
     return (
         <div className='container mx-auto'>
-            <h1 className='text-5xl'>Meus repositórios eu sou o Tulio Faria</h1>
+            <h1 className='text-5xl'>Meus repositórios eu sou o Paulo Henrique Barbosa Santos</h1>
             <h2 className='font-bold text-3xl'>Meus repositórios no GitHub</h2>
             <p>Github stats: public repos: {user.public_repos} / public_gists: {user.public_gists} / followers: {user.followers}</p>
             {repos.map(repo => {
