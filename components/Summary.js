844c3677ac6138479f8c74c4bee7203889e3dc32
phprograming <paulo_hbs17@hotmail.com>
2022-03-05 15:39:07 -0300

    Alter info

--- components/Summary.js
+++ components/Summary.js
@@ -6,7 +6,7 @@ const Summary = () => {
       <h3 className='text-4xl font-bold text-center uppercase mt-10 mb-5 text-blue-500'>About me</h3>
       <div className='bg-white rounded-lg shadow-lg py-12 px-8 mx-8 md:mx-0 md:px-16'>
         <p className='text-2xl'>
-          I am a 22 year old who is passionate about technology! I am constantly evolving in the area of WEB Development, Mobile and Backend. Currently I work as a PHP Developer, every day improving myself on current technologies such as: ReactJS, React Native and NodeJS.
+          Loading  
         </p>
       </div>
     </div>
